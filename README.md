87184bdaf6a184cd9f6a8678d02b699760fbc720
alvarezmarco <alvarez_marco@hotmail.com>
2017-07-09 12:12:44 +0200

    comentario

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Estos son algunos scripts escritos en Python.
 
+informacionDNS.py : Este script consulta los registros DNS sobre IPv4 (del tipo A), IPv6 (del tipo AAAA), Name Serveres (NS), MailServers (MX)\permite 
+Uso: python  informacionDNS.py dominio. EX:  python informacionDNS.py google.es
+
+Geolocalizacion.py : permite consultar las coordenadas y ubicación de un dominio. 
+
+metadatospdf.py: Analiza los metadatos de un archivo PDF, de utilidad para el Análisis Forense
+
--- informacionDNS.py
+++ informacionDNS.py
@@ -1,16 +1,52 @@
+# Este script consulta los registros DNS sobre IPv4 (del tipo A), IPv6 (del tipo AAAA),
+# Name Serveres (NS), MailServers (MX)\
+# Uso python informacionDNS.py -d nombredeldominio
+# Ejemplo python informacionDNS.py -d nombredeldominio
+# Autor: Marco Alvarez Murillo.
+
 import dns
 import dns.resolver
-tipoA=(dns.resolver.query('google.com','A'))
-tipoMX=(dns.resolver.query('google.com','MX'))
-tipoNS=(dns.resolver.query('google.com','NS'))
-tipoAAA=(dns.resolver.query('google.com','AAAA'))
-print tipoA.response
-print tipoMX.response
-print tipoNS.response
-print tipoAAA.response
+import argparse
 
+def resolverdns (dom):
+  try:
+    
+     tipoA=(dns.resolver.query(dom,'A'))
+     tipoMX=(dns.resolver.query(dom,'MX'))
+     tipoNS=(dns.resolver.query(dom,'NS'))
+     tipoAAA=(dns.resolver.query(dom,'AAAA'))
+     
+     print tipoA.response
+     print tipoMX.response
+     print tipoNS.response
+     print tipoAAA.response
+  
+  except Exception, e:
+    print (e)
 
-
-
-
-
+def main ():
+    try:
+     parser = argparse.ArgumentParser()
+     
+     #Se define el argumento a ingresar por consola, en este cado d y dominio
+     parser.add_argument("-d", "--dominio" ,help="Ingresar un nombre de dominio")
+     
+     #Se pueden incluir mas opciones en est caso -a para ayuda.
+     parser.add_argument("-a", "--ayuda", help="", action="store_true" )
+     #Se pasan todos los argumentos a args.
+     args = parser.parse_args()
+     
+     #Se comprueba si se el usuario escoge -d.
+     if args.dominio :
+        domin=args.dominio
+        resolverdns(domin)
+    
+     # Ayuda HELP.
+     if args.ayuda :
+           print("Uso:  [OPTIONS]")
+           print ("   -d Ingresar un nombre de dominio. Ex: python informacionDNS.py -d google.es ")
+           print ("   -a Ayuda.")
+    except Exception, e:
+      print (e)  
+if __name__ == "__main__":
+  main()
